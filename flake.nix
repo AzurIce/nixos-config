7d7ec2c1620a40025f448b18fce153e82ee485f8
AzurIce <973562770@qq.com>
2023-01-01 12:02:03 +0800

    added ntfs-3g, restructured the code

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       system = "x86_64-linux";
       modules = [
         hyprland.nixosModules.default
-        ./hosts.configuration.nix
+        ./hosts/configuration.nix
       ];
     };
   };
