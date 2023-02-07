2b925b0590b75bdfe01bb48751ab93be3ba2f20e
AzurIce <973562770@qq.com>
2023-02-07 19:58:17 +0800

    clean up

--- modules/desktop/hyprland/home.nix
+++ modules/desktop/hyprland/home.nix
@@ -1,44 +1,44 @@
 { pkgs, user, ... }:
 
 {
-    home.file.".config/hypr".source = ./hypr;
+  home.file.".config/hypr".source = ./hypr;
 
-    home.sessionVariables = {
-      EDITOR = "nvim";
-      # BROWSER = "firefox";
-      TERMINAL = "kitty";
-      QT_QPA_PLATFORMTHEME = "gtk3";
-      QT_SCALE_FACTOR = "1";
-      MOZ_ENABLE_WAYLAND = "1";
-      SDL_VIDEODRIVER = "wayland";
-      _JAVA_AWT_WM_NONREPARENTING = "1";
-      QT_QPA_PLATFORM = "wayland";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-      WLR_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card0";
+  # Note: the sessionVariables will be only set when home-manager manager a shell
+  # e.g. fish, bash
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    BROWSER = "firefox";
+    TERMINAL = "kitty";
 
-      #GBM
-      #LIBVA_DRIVER_NAME = "nvidia";
-      #GBM_BACKEND = "nvidia-drm";
-      #CLUTTER_BACKEND = "wayland";
-      #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      #WLR_NO_HARDWARE_CURSORS = "1";
+    QT_QPA_PLATFORMTHEME = "gtk3";
+    QT_SCALE_FACTOR = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    SDL_VIDEODRIVER = "wayland";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
 
-      #vulkan
-      #WLR_RENDERER = "vulkan";
-      #__NV_PRIME_RENDER_OFFLOAD="1";
+    #GBM
+    #LIBVA_DRIVER_NAME = "nvidia";
 
-      XDG_CURRENT_DESKTOP = "Hyprland";
-      XDG_SESSION_DESKTOP = "Hyprland";
-      XDG_SESSION_TYPE = "wayland";
-      XDG_CACHE_HOME = "\${HOME}/.cache";
-      XDG_CONFIG_HOME = "\${HOME}/.config";
-      XDG_BIN_HOME = "\${HOME}/.local/bin";
-      XDG_DATA_HOME = "\${HOME}/.local/share";
-    };
-#    sessionPath = [
-#      "$HOME/.npm-global/bin"
-#      "$HOME/.local/bin"
-#      "$HOME/Codelearning/go/bin"
-#    ];
+    # Force using GBM backend
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+
+    #CLUTTER_BACKEND = "wayland";
+    #WLR_NO_HARDWARE_CURSORS = "1";
+
+    #vulkan
+    #WLR_RENDERER = "vulkan";
+    #__NV_PRIME_RENDER_OFFLOAD="1";
+
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_CACHE_HOME = "\${HOME}/.cache";
+    XDG_CONFIG_HOME = "\${HOME}/.config";
+    XDG_BIN_HOME = "\${HOME}/.local/bin";
+    XDG_DATA_HOME = "\${HOME}/.local/share";
+  };
 }
