4ce11559b40e16e08c60ccbd3fc71995d60aeac6
AzurIce <973562770@qq.com>
2023-02-07 14:32:29 +0800

    fixed

--- hosts/laptop-blade/default.nix
+++ hosts/laptop-blade/default.nix
@@ -8,7 +8,7 @@ let
   launch-hyprland = pkgs.writeShellScriptBin "launch-hyprland" ''
     export LIBVA_DRIVER_NAME=nvidia
     export XDG_SESSION_TYPE=wayland
-    export GBM_BACKEND=nvidia-drm
+    #export GBM_BACKEND=nvidia-drm
     export __GLX_VENDOR_LIBRARY_NAME=nvidia
     export WLR_NO_HARDWARE_CURSORS=1
 
