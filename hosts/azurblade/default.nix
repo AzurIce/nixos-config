f619a1782e0c198a4e25ff33b4b67ed5f5e7aaac
AzurIce <973562770@qq.com>
2023-06-07 12:40:40 +0800

    fresh azurblade

---
+++ hosts/azurblade/default.nix
@@ -0,0 +1,16 @@
+{ user, nixpkgs, hyprland, home-manager, nvim-config, nur, ... }:
+
+nixpkgs.lib.nixosSystem {
+  system = "x86_64-linux";
+  specialArgs = { inherit user hyprland home-manager nvim-config; };
+  modules = [
+    ./configuration.nix
+    nur.nixosModules.nur
+    home-manager.nixosModules.home-manager {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.extraSpecialArgs = { inherit user nvim-config; };
+      home-manager.users.${user} = import ./home.nix;
+    }
+  ];
+}
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,6 +1,9 @@
 { nixpkgs, hyprland, home-manager, nvim-config, nur, nixos-wsl, ... }:
 
 let user = "azurice"; in {
+  azurblade = import ./azurblade {
+      inherit user nixpkgs hyprland home-manager nvim-config nur;
+  };
   laptop-blade = import ./laptop-blade {
       inherit user nixpkgs hyprland home-manager nvim-config nur;
   };
