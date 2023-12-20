cc1b3fcf42c283fe056ce2a620603245e35f7de4
Azur冰弦 <973562770@qq.com>
2023-12-20 15:40:41 +0800

    added oh-my-zsh to darwin

--- hosts/AzurMac/home.nix
+++ hosts/AzurMac/home.nix
@@ -21,7 +21,32 @@
 
   programs.zsh = {
     enable = true;
+    enableAutosuggestions = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+        "ag"
+        "aliases"
+        # "colored-man-page"
+        "colorize"
+        "copypath"
+        # "zsh-autosuggestions"
+      ];
+    };
   };
+
+  # programs.starship = {
+  #   enable = true;
+  #   # 自定义配置
+  #   settings = {
+  #     add_newline = false;
+  #     # aws.disabled = true;
+  #     # gcloud.disabled = true;
+  #     # line_break.disabled = true;
+  #   };
+  # };
 
   home.stateVersion = "23.11";
   programs.home-manager.enable = true;
