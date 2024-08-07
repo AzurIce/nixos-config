5a2024f2483c84300d00edf648f6b17f4812fc24
AzurIce <973562770@qq.com>
2024-08-07 17:36:21 +0800

    added gcm as git's credential helper

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -2,13 +2,19 @@ inputs@{ pkgs, user, ... }:
 
 {
   programs.git = {
-      enable = true;
-      userEmail = "973562770@qq.com";
-      userName = "AzurIce";
-      extraConfig = {
-        http.proxy = "http://127.0.0.1:7890";
-        https.proxy = "https://127.0.0.1:7890";
-        safe.directory = "*";
+    enable = true;
+    userEmail = "973562770@qq.com";
+    userName = "AzurIce";
+    extraConfig = {
+      http.proxy = "http://127.0.0.1:7890";
+      https.proxy = "https://127.0.0.1:7890";
+      safe.directory = "*";
+      credential = {
+        credentialStore = "secretservice";
+        helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
       };
+    };
+
+    delta.enable = true;
   };
 }
